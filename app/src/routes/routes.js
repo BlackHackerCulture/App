33d0465ad6477c6135f6384d1754799c1ca22d67
Bosbacl_Bobur <bosbacl713@gmail.com>
2025-10-05 22:50:26 +0500

    add

--- app/src/routes/routes.js
+++ app/src/routes/routes.js
@@ -1,15 +1,13 @@
-import { Home } from "../pages/Home"
-import { About } from "../pages/About"
-import { Contact } from "../pages/Contact"
-import { Login } from "../pages/Login"
-import { Register } from "../pages/Register"
-
-
+import { Home } from "../pages/Home";
+import { About } from "../pages/About";
+import { Contact } from "../pages/Contact";
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
 
 export const routes = [
-  { path: "/", element: <Home /> },
-  { path: "/about", element: <About /> },
-  { path: "/contact", element: <Contact /> },
-  { path: "/login", element: <Login /> },
-  { path: "/register", element: <Register /> },
-]
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+  { path: "/contact", component: Contact },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register },
+];
