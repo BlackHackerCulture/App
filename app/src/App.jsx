b6b1f1672eb420f8e0d80e9248fa0f55a2d0a73d
Bosbacl_Bobur <bosbacl713@gmail.com>
2025-10-05 22:04:02 +0500

    add

---
+++ app/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+
+      <main className="flex-grow">
+        <Home />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
